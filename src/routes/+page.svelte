028e24abcb542b6f04060006a9d7ab7095553850
Maximo Angel Verzini Davico <maximoverzini@gmail.com>
2023-03-07 13:29:40 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+
+<section aria-label="Contenido principal" class="w-full h-full bg-slate-50 p-10">
+    
+    <div class="flex items-center space-x-10 text-lg font-fira">
+        <div class="h-16 w-16 bg-slate-500"></div>
+
+        <a data-sveltekit-preload-data="tap" href='/'>
+            Inicio 
+        </a>
+        
+        <a data-sveltekit-preload-data="tap" href='/whoami'>
+            ¿Quien soy?
+        </a>
+
+        <a data-sveltekit-preload-data="tap" href='/proyects'>
+            Proyectos
+        </a>
+
+        <a data-sveltekit-preload-data="tap" href="/contact">
+            Contacto
+        </a>
+
+    </div>
+
+    <div class="flex space-x-10">
+        <h1 class="font-alfa text-5xl items-center" >Maximo Angel Verzini Davico</h1>
+        <div class="h-16 w-16 bg-slate-500"></div>
+        <div class="h-16 w-16 bg-slate-500"></div>
+        <div class="h-16 w-16 bg-slate-500"></div>
+        <div class="h-16 w-16 bg-slate-500"></div>
+        <div class="h-16 w-16 bg-slate-500"></div>
+    </div>
+</section>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'alfa' : 'Alfa Slab One',
+        'fira' : 'Fira Sans',
+      }
+    },
+  },
+  plugins: [],
+}
