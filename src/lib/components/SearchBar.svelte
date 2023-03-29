650dded2cb6ec9276678af31313f728bf4530106
Maximo Angel Verzini Davico <maximoverzini@gmail.com>
2023-03-29 10:09:56 -0300

    feat(wip): home page src added

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<div class="h-14 border-slate-900 border-2 flex items-center px-6 rounded-lg text-xl">
+	<i class="material-icons mr-5 select-none" style="font-size: 1.5rem;"> search </i>
+	<input
+		type="text"
+		class="placeholder:text-slate-400 bg-transparent focus:outline-none basis-full w-full"
+		placeholder="Search packages" />
+	<div class="h-6 border-l border-slate-900"></div>
+	<i class="material-icons ml-5 select-none">tune</i>
+</div>
